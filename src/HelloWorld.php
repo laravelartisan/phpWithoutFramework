756372752da4ec61ca5a9b4d4b3d5dd6f69653a5
Nazim <rajaftfl@outlook.com>
2018-03-24 19:12:20 +0600

    components used to create the first route

--- src/HelloWorld.php
+++ src/HelloWorld.php
@@ -1,14 +1,46 @@
 <?php
-
 declare(strict_types=1);
-
 namespace ExampleApp;
-
+use Psr\Http\Message\ResponseInterface;
+/**
+ *
+ *
+ * @package   ExampleApp
+ */
 class HelloWorld
 {
-    public function announce(): void
+    /**
+     * @var string
+     */
+    private $foo;
+    /**
+     * @var ResponseInterface
+     */
+    private $response;
+    /**
+     * HelloWorld constructor.
+     *
+     * @param ResponseInterface $response
+     * @param string $foo
+     */
+    public function __construct(
+        string $foo,
+        ResponseInterface $response
+    ) {
+        $this->foo = $foo;
+        $this->response = $response;
+    }
+    /**
+     *
+     *
+     * @throws \RuntimeException
+     * @throws \InvalidArgumentException
+     */
+    public function __invoke(): ResponseInterface
     {
-        echo 'Hello, autoloaded world!';
-        exit;
+        $response = $this->response->withHeader('Content-Type', 'text/html');
+        $response->getBody()
+            ->write("<html><head></head><body>Hello, {$this->foo} world!</body></html>");
+        return $response;
     }
 }
